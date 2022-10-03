bfddc1caa1d020bc20f826b15238f0e1b6dcf036
Michael <mkhudson17@my.trine.edu>
2022-10-03 17:38:13 -0400

    Create Matrix_Pong_Display.c
    Initial Code for display driver

---
+++ Core/Src/Matrix_Pong_Display.c
@@ -0,0 +1,45 @@
+/*
+ * Matrix_Pong_Display.c
+ *
+ *  Created on: Oct 3, 2022
+ *      Author: michael Hudson
+ */
+
+/*GPIOC->ODR = 255; //SET GPIOC ENTIRE REGISTER HIGH
+GPIOB->ODR = 0;	//SET GPIOB ENTIRE REGISTER LOW*/
+#include "main.h"
+void Matrix_LED_DISPLAY_PONG( int L_PADDLE_Y_COORDINATE, int R_PADDLE_Y_COORDINATE, int BALL_X_COORDINATE, int BALL_Y_COORDINATE)
+{//Start Function
+		  for (volatile int LED_MATRIX_COLUMN = 0; LED_MATRIX_COLUMN <= 7; LED_MATRIX_COLUMN++)
+		  {		//Begin for LOOP
+			GPIOC->ODR &=~(1<<LED_MATRIX_COLUMN); 			//ENABLE CURRENT LED COLUMN
+			//if (LED_MATRIX_COLUMN == 0) // display left paddle
+			//{//Begin if 1
+
+				//	if (BALL_X_COORDINATE == 0) // Check to see if ball is on paddle coordinate
+				//{//Begin if 1.2
+
+				//}//End if 1.2
+
+			//}//End if 1
+
+		//	if (LED_MATRIX_COLUMN == BALL_X_COORDINATE)
+			//{//Begin if 2
+
+			//}//End if 2
+
+		//	if (LED_MATRIX_COLUMN == 7) // display right paddle
+			//{//Begin if 3
+
+				//	if (BALL_X_COORDINATE == 7) // Check to see if ball is on paddle coordinate
+				//{//Begin if 3.2
+
+				//}//End if 3.2
+
+			//}//End if 3
+
+
+			GPIOC->ODR |= (1<<LED_MATRIX_COLUMN); 		 //DISABLE CURRENT LED COLUMN
+			GPIOB->ODR = 0;								 //RESET ROW
+		  }  //END For Loop
+}//End Function
