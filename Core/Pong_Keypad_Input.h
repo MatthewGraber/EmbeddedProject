4646671f0e43cb8079921dc4ed94871856874913
zabrickler <113313618+zabrickler@users.noreply.github.com>
2022-10-03 20:50:11 -0400

    Add files via upload
    The Keypad Driver. Not Complete

---
+++ Core/Pong_Keypad_Input.h
@@ -0,0 +1,23 @@
+/*
+ * Quadknob.h
+ *
+ *  Created on: Oct 3, 2022
+ *      Author: Zach Brickler
+ *
+ *   Contains DebouncedEdgeDetector and QuadKnob
+ */
+
+#ifndef INC_PONG_KEYPAD_INPUT_H_
+#define INC_PONG_KEYPAD_INPUT_H_
+
+#include "main.h"
+
+
+// Methods for QuadKnob
+void keypad_Inputs();
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+#endif /* INC_PONG_KEYPAD_INPUT_H_ */
