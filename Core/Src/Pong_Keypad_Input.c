85771bc6de07c506e2688cac150a67081c0e1d83
MatthewGraber <mbgraber19@my.trine.edu>
2022-10-10 19:17:34 -0400

    Type conversion
    Fixed some type errors

--- Core/Src/Pong_Keypad_Input.c
+++ Core/Src/Pong_Keypad_Input.c
@@ -16,12 +16,12 @@
 //int paddle2; // -1 = down, 0 = still, 1 = up
 uint32_t second;
 uint32_t first;
-uint16_t state;
+Q_data state;
 
 void Pong_Keypad_Input_init(Pong_Keypad_Input *self){
 	self->update = &keypad_Inputs;
 	self->get = Pong_Keypad_Input_get;
-	state = 0x0;
+	state.int_val = 0x0;
 }
 
 
@@ -49,25 +49,23 @@ void keypad_Inputs(Pong_Keypad_Input *self, Smc_queue *pass){
 
 		second = GPIOA->IDR;
 
+		state.int_val = 0x0;
+
 		if((first == 0x10) && (second == 0x4030)){         //Button 1
-			state = 0x1;
+			state.int_val += 0x1;
 		}
 		if((first == 0x4000) && (second == 0x4030)){         //Button 7
-			state = 0x2;
+			state.int_val += 0x2;
 		}
-		if((first == 0x4018) && (second == 0x4030)){
-				state = 0x0;
-			}
+
 
 		if((first == 0x4018) && (second == 0x10)){         //Button 3
-					state = 0x4;
+					state.int_val += 0x4;
 			}
 			if((first == 0x4018) && (second == 0x4000)){         //Button 9
-				state = 0x8;
+				state.int_val += 0x8;
 			}
-			if((first == 0x4010) && (second == 0x4010)){
-				state = 0x0;
-			}
+
 		self->push = state;
 		smc_queue.put(pass, state);
 
